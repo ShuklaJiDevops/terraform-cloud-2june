a89361bc6d1e38e391bb4a2b388a723b3c11f5fa
ShuklaJiDevops <88495442+ShuklaJiDevops@users.noreply.github.com>
2023-06-02 12:18:26 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,6 @@ provider "aws" {
 resource "aws_instance" "TEST" {
 ami = "ami-0715c1897453cabd1"
 instance_type = "t2.micro"
-vpc_security_group_ids = [aws_security_group.new-sg.id]
 }
 
 resource "aws_eip" "myeip" {
